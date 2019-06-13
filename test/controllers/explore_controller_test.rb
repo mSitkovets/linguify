dcfdb223d41864ca512d844ae1aacbc7c8139bc2
Maria Sitkovets <maria.sitkovets@shopify.com>
2019-06-13 14:22:30 -0400

    Cached partial results

--- test/controllers/explore_controller_test.rb
+++ test/controllers/explore_controller_test.rb
@@ -4,6 +4,9 @@ class ExploreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get explore_index_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 3
+    assert_select 'main ul.catalog li', 3
+    assert_select 'h2', 'Foods in Swahili' 
   end
 
 end
