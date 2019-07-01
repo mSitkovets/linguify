b356105b7f6dee28e2e0a24c8bb90670d8041b3d
Maria Sitkovets <maria.sitkovets@shopify.com>
2019-07-01 12:47:15 -0400

    Added a selector to switch between languages

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -3,6 +3,10 @@ class ExploreController < ApplicationController
   before_action :set_list
   skip_before_action :authorize
   def index
-    @quizzes = Quiz.order(:title)
+    if params[:set_locale]
+      redirect_to explore_index_url(locale: params[:set_locale]) 
+    else
+      @quizzes = Quiz.order(:title) 
+    end
   end
 end
